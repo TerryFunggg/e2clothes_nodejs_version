1977f7659bd1bac5555c2ea9e76c6b1556e79fe8
TerryFunggg <terryyessfung@gmail.com>
2021-04-30 01:19:07 +0800

    fix tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   mode: 'jit',
   purge: [
-    './src/*.html',
-   './src/*.js',
+    './src/**/*.html',
+   './src/**/*.js',
     'views/**/*.pug'
   ],
   darkMode: false, // or 'media' or 'class'
