1a74e2b3338bfa82261546d65238ca66db061da2
TerryFunggg <terryyessfung@gmail.com>
2021-05-04 22:06:00 +0800

    style & feat market

---
+++ server/controllers/market_controller.js
@@ -0,0 +1,34 @@
+const gqlFetch = require("../myGrapgQLFetch");
+const Validation = require("../validation");
+
+exports.index = async (req, res) => {
+  let domEl = {
+    market:true,
+    page_title: "Market",
+    user_menus: ["Your Profile", "Your Order"]
+  };
+
+  if (!req.cookies.token) return res.render("pages/market", domEl);
+
+  let token = req.cookies.token
+  let data = await gqlFetch(`
+   {
+      me {
+        firstName
+        lastName
+        userName
+        role
+        avatar
+      }
+    }
+  `, token);
+
+  if (!!data?.data?.me) {
+    domEl.user = data.data.me;
+
+    const role = data.data.me
+    domEl.user_menus.push( role === 'seller' ? 'Your shop' : 'Apply Shop' )
+  }
+
+  res.render("pages/market", domEl);
+}
--- server/routes.js
+++ server/routes.js
@@ -6,6 +6,7 @@ const pug = require("pug");
 // Controllers
 const Home = require('./controllers/home_controller')
 const Account = require('./controllers/account_controller');
+const Market = require('./controllers/market_controller');
 
 
 // Route section
@@ -14,5 +15,6 @@ router.get("/login", Account.login)
 router.post("/login", Account.verifyLogin)
 router.get("/signup", Account.signup)
 router.post("/signup", Account.verifySignUp)
+router.get("/market", Market.index)
 
 module.exports = router;
